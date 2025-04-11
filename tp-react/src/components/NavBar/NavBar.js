2387832c020388e8eec5f5440ecfd67119302bda
FacundoStorani <storanif@udesa.edu.ar>
2025-04-11 20:21:49 -0300

    Arreglos

---
+++ tp-react/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = (props) => {
+  const { elemento, link } = props;
+  return (
+    <li>
+      <a className="textoNavBar" href={`${link}`}>
+        {elemento}
+      </a>
+    </li>
+  );
+};
+
+export default NavBar;
